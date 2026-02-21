a82393485e9799b5d00a60725259ed450b48e071
sreedharperim <sreedharperim@yahoo.com>
2026-02-21 10:21:18 +0530

    Change Router import to use HashRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import App from "./App";
